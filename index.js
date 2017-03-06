4b2f800574e91be0cc6509f923a4316a8fe7f492
jamesw <jamesw@deeson.co.uk>
2017-03-06 19:34:57 +0000

    webpack 2

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const WriteFilePlugin = require('write-file-webpack-plugin');
 const DrupalTemplatePlugin = require('./drupal-templates-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 
-const extractCSS = new ExtractTextPlugin('[name].css');
+process.noDeprecation = true;
 
 const path = require('path');
 
 const config = {
   entry: {
-    app: ['./src/app.js'],
+    app: './src/app.js',
+    pages: './pages/index.js',
   },
   devtool: '#source-map',
   output: {
     path: path.resolve(process.cwd(), 'assets'),
     publicPath: 'needs-to-be-set',
-    filename: 'app.js',
+    filename: '[name].js',
   },
   devServer: {
     inline: true,
     quiet: false,
     noInfo: false,
+    https: true,
     stats: {
       assets: false,
       colors: true,
@@ -30,7 +33,6 @@ const config = {
       chunks: false,
       chunkModules: false,
     },
-    outputPath: path.join(process.cwd(), 'assets'),
     proxy: {
       '*': {
         target: 'http://localhost:3000',
@@ -39,40 +41,58 @@ const config = {
     },
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
         loader: 'babel-loader',
-        query: {
+        options: {
           presets: ['es2015'],
         },
       },
       {
         test: /\.scss$/,
-        loader: extractCSS.extract(['css-loader', 'postcss-loader', 'sass-loader']),
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader', 'sass-loader'],
+        }),
       },
       {
         test: /.*\.(gif|png|jpe?g|svg)(\?v=\d+\.\d+\.\d+)?$/i,
-        loaders: [
-          'url?limit=10000&hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}',
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              hash: 'sha512',
+              digest: 'hex',
+              name: '[hash].[ext]',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              pngquant: { quality: '65-90', speed: 4 },
+              mozjpeg: { progressive: true },
+              gifsicle: { interlaced: false },
+            },
+          },
         ],
       },
-      { test: /\.(tpl\.php|html\.twig)$/, loaders: ['file?regExp=.*\/src\/(.*)&name=[1]'], exclude: [/pages/] },
-      { test: /(\.php|\.twig)$/, loaders: ['file?name=pages/[name].[ext]'], exclude: [/src/] },
+      { test: /\.(tpl\.php|html\.twig)$/, loader: 'file-loader', options: { regExp: '.*/src/(.*)', name: '[1]' }, exclude: [/pages/] },
+      { test: /(\.php|\.twig)$/, loader: 'file-loader', options: { name: 'pages/[name].[ext]' }, exclude: [/src/] },
 
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-      { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
+      { test: /\.(woff|woff2)$/, loader: 'url-loader', options: { prefix: 'font/', limit: 5000 } },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader', options: { limit: 10000, mimetype: 'application/octet-stream' } },
     ],
   },
   plugins: [
     new WriteFilePlugin({ log: false }),
     new DrupalTemplatePlugin(),
-    extractCSS,
+    new ExtractTextPlugin('[name].css'),
+    new webpack.LoaderOptionsPlugin({ options: { postcss: [autoprefixer] } }),
   ],
-  postcss: () => [autoprefixer],
 };
 
 module.exports = config;
