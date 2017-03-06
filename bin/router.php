285e3adb57f3a48f86e1010851a00038299f4a8c
jamesw <jamesw@deeson.co.uk>
2017-03-06 15:16:42 +0000

    starter for component generating, but weak

--- bin/router.php
+++ bin/router.php
@@ -1,5 +1,11 @@
 <?php
 
+if (!file_exists($_SERVER['DOCUMENT_ROOT'] . $_SERVER['REQUEST_URI'])) {
+  header("HTTP/1.0 404 Not Found");
+  print '<h1>404 Not Found</h1>';
+  exit;
+}
+
 if (preg_match('/\.(?:twig)$/', $_SERVER["REQUEST_URI"])) {
   require_once './vendor/autoload.php';
 
@@ -14,12 +20,18 @@ if (preg_match('/\.(?:twig)$/', $_SERVER["REQUEST_URI"])) {
 } else {
   function render($t) { return $t; }
   function theme($n, $content = ['content'=>[]]) {
+    $path = str_replace(' ', '', lcfirst(ucwords(str_replace('_', ' ', $n))));
+    $name = str_replace('_', '-', $n);
+    $file = $_SERVER['DOCUMENT_ROOT'] . '/src/components/'.$path.'/'.$name.'.tpl.php';
+
+    if (!file_exists($file)) {
+      return "<pre style='background-color: #e2e2e2; padding: 0.5em; color: #666'>missing component template '$n'</pre>";
+    }
+
     ob_start();
 
     $content = $content['content'];
-    $path = str_replace(' ', '', lcfirst(ucwords(str_replace('_', ' ', $n))));
-    $name = str_replace('_', '-', $n);
-    include $_SERVER['DOCUMENT_ROOT'] . '/src/components/'.$path.'/'.$name.'.tpl.php';
+    include $file;
 
     $out = ob_get_contents();
     ob_end_clean();
